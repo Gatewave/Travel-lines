c61b84da246696ac5a61ee78549f533e83e5c743
mohamed <mohamed.alaa.el.din.hassan@gmail.com>
2018-07-16 18:37:00 +0200

    first commit

--- js/plugins.js
+++ js/plugins.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#countries").msDropdown();
     // $('#myModal').modal('toggle');
+
 });
 $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
@@ -9,8 +10,19 @@ $(window).on('scroll', function() {
     if(y_scroll_pos > scroll_pos_test) {
         $("#categories").fadeOut(200,"swing");
         $("#toggle_menu").removeClass("d-none");
+        $("nav.navbar.navbar-expand-lg.navbar-light.bg-light").css({
+            "position":"fixed",
+            "top":"0",
+            "z-index":"999",
+            "width":"100%",
+            "transition":"all ease-in-out 0.5s"
+        });
+
     }else{
         $("#categories").fadeIn(2000,"swing");
         $("#toggle_menu").removeClass("d-none");
+        $("nav.navbar.navbar-expand-lg.navbar-light.bg-light").css({
+            "position":"relative",
+        });
     }
 });
